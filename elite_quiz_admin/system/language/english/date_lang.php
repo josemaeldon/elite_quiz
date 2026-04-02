fb88f05ab04e49614154fe2726d2c86002805320
José Maeldon da Silva <54424205+josemaeldon@users.noreply.github.com>
2026-04-02 12:35:42 -0300

    Translate date_lang.php content to Portuguese
    Translated license and author information to Portuguese.

--- elite_quiz_admin/system/language/english/date_lang.php
+++ elite_quiz_admin/system/language/english/date_lang.php
@@ -2,93 +2,92 @@
 /**
  * CodeIgniter
  *
- * An open source application development framework for PHP
+ * Um framework de desenvolvimento de aplicações open source para PHP
  *
- * This content is released under the MIT License (MIT)
+ * Este conteúdo está sob a licença MIT (MIT)
  *
  * Copyright (c) 2014 - 2019, British Columbia Institute of Technology
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+ * Permissão é concedida, gratuitamente, a qualquer pessoa que obtenha uma cópia
+ * deste software e dos arquivos de documentação associados (o "Software"), para lidar
+ * com o Software sem restrição, incluindo, sem limitação, os direitos
+ * de usar, copiar, modificar, mesclar, publicar, distribuir, sublicenciar e/ou vender
+ * cópias do Software, e permitir que as pessoas a quem o Software é
+ * fornecido o façam, sujeito às seguintes condições:
  *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
+ * O aviso de copyright acima e este aviso de permissão devem ser incluídos em
+ * todas as cópias ou partes substanciais do Software.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+ * O SOFTWARE É FORNECIDO "COMO ESTÁ", SEM GARANTIA DE QUALQUER TIPO, EXPRESSA OU
+ * IMPLÍCITA, INCLUINDO, MAS NÃO SE LIMITANDO ÀS GARANTIAS DE COMERCIALIZAÇÃO,
+ * ADEQUAÇÃO A UM DETERMINADO FIM E NÃO VIOLAÇÃO. EM NENHUM CASO OS
+ * AUTORES OU DETENTORES DE DIREITOS AUTORAIS SERÃO RESPONSÁVEIS POR QUALQUER RECLAMAÇÃO, DANOS OU OUTROS
+ * RESPONSABILIDADE, SEJA EM UMA AÇÃO DE CONTRATO, ATO ILÍCITO OU DE OUTRA FORMA, DECORRENTE DE,
+ * FORA DE OU EM CONEXÃO COM O SOFTWARE OU O USO OU OUTRAS NEGOCIAÇÕES NO SOFTWARE.
  *
  * @package	CodeIgniter
- * @author	EllisLab Dev Team
+ * @author	Equipe EllisLab
  * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (https://ellislab.com/)
  * @copyright	Copyright (c) 2014 - 2019, British Columbia Institute of Technology (https://bcit.ca/)
- * @license	https://opensource.org/licenses/MIT	MIT License
+ * @license	https://opensource.org/licenses/MIT	Licença MIT
  * @link	https://codeigniter.com
- * @since	Version 1.0.0
+ * @since	Versão 1.0.0
  * @filesource
  */
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') OR exit('Acesso direto ao script não é permitido');
 
-$lang['date_year'] = 'Year';
-$lang['date_years'] = 'Years';
-$lang['date_month'] = 'Month';
-$lang['date_months'] = 'Months';
-$lang['date_week'] = 'Week';
-$lang['date_weeks'] = 'Weeks';
-$lang['date_day'] = 'Day';
-$lang['date_days'] = 'Days';
-$lang['date_hour'] = 'Hour';
-$lang['date_hours'] = 'Hours';
-$lang['date_minute'] = 'Minute';
-$lang['date_minutes'] = 'Minutes';
-$lang['date_second'] = 'Second';
-$lang['date_seconds'] = 'Seconds';
+$lang['date_year'] = 'Ano';
+$lang['date_years'] = 'Anos';
+$lang['date_month'] = 'Mês';
+$lang['date_months'] = 'Meses';
+$lang['date_week'] = 'Semana';
+$lang['date_weeks'] = 'Semanas';
+$lang['date_day'] = 'Dia';
+$lang['date_days'] = 'Dias';
+$lang['date_hour'] = 'Hora';
+$lang['date_hours'] = 'Horas';
+$lang['date_minute'] = 'Minuto';
+$lang['date_minutes'] = 'Minutos';
+$lang['date_second'] = 'Segundo';
+$lang['date_seconds'] = 'Segundos';
 
-$lang['UM12']	= '(UTC -12:00) Baker/Howland Island';
+$lang['UM12']	= '(UTC -12:00) Ilhas Baker/Howland';
 $lang['UM11']	= '(UTC -11:00) Niue';
-$lang['UM10']	= '(UTC -10:00) Hawaii-Aleutian Standard Time, Cook Islands, Tahiti';
-$lang['UM95']	= '(UTC -9:30) Marquesas Islands';
-$lang['UM9']	= '(UTC -9:00) Alaska Standard Time, Gambier Islands';
-$lang['UM8']	= '(UTC -8:00) Pacific Standard Time, Clipperton Island';
-$lang['UM7']	= '(UTC -7:00) Mountain Standard Time';
-$lang['UM6']	= '(UTC -6:00) Central Standard Time';
-$lang['UM5']	= '(UTC -5:00) Eastern Standard Time, Western Caribbean Standard Time';
-$lang['UM45']	= '(UTC -4:30) Venezuelan Standard Time';
-$lang['UM4']	= '(UTC -4:00) Atlantic Standard Time, Eastern Caribbean Standard Time';
-$lang['UM35']	= '(UTC -3:30) Newfoundland Standard Time';
-$lang['UM3']	= '(UTC -3:00) Argentina, Brazil, French Guiana, Uruguay';
-$lang['UM2']	= '(UTC -2:00) South Georgia/South Sandwich Islands';
-$lang['UM1']	= '(UTC -1:00) Azores, Cape Verde Islands';
-$lang['UTC']	= '(UTC) Greenwich Mean Time, Western European Time';
-$lang['UP1']	= '(UTC +1:00) Central European Time, West Africa Time';
-$lang['UP2']	= '(UTC +2:00) Central Africa Time, Eastern European Time, Kaliningrad Time';
-$lang['UP3']	= '(UTC +3:00) Moscow Time, East Africa Time, Arabia Standard Time';
-$lang['UP35']	= '(UTC +3:30) Iran Standard Time';
-$lang['UP4']	= '(UTC +4:00) Azerbaijan Standard Time, Samara Time';
-$lang['UP45']	= '(UTC +4:30) Afghanistan';
-$lang['UP5']	= '(UTC +5:00) Pakistan Standard Time, Yekaterinburg Time';
-$lang['UP55']	= '(UTC +5:30) Indian Standard Time, Sri Lanka Time';
-$lang['UP575']	= '(UTC +5:45) Nepal Time';
-$lang['UP6']	= '(UTC +6:00) Bangladesh Standard Time, Bhutan Time, Omsk Time';
-$lang['UP65']	= '(UTC +6:30) Cocos Islands, Myanmar';
-$lang['UP7']	= '(UTC +7:00) Krasnoyarsk Time, Cambodia, Laos, Thailand, Vietnam';
-$lang['UP8']	= '(UTC +8:00) Australian Western Standard Time, Beijing Time, Irkutsk Time';
-$lang['UP875']	= '(UTC +8:45) Australian Central Western Standard Time';
-$lang['UP9']	= '(UTC +9:00) Japan Standard Time, Korea Standard Time, Yakutsk Time';
-$lang['UP95']	= '(UTC +9:30) Australian Central Standard Time';
-$lang['UP10']	= '(UTC +10:00) Australian Eastern Standard Time, Vladivostok Time';
-$lang['UP105']	= '(UTC +10:30) Lord Howe Island';
-$lang['UP11']	= '(UTC +11:00) Srednekolymsk Time, Solomon Islands, Vanuatu';
-$lang['UP115']	= '(UTC +11:30) Norfolk Island';
-$lang['UP12']	= '(UTC +12:00) Fiji, Gilbert Islands, Kamchatka Time, New Zealand Standard Time';
-$lang['UP1275']	= '(UTC +12:45) Chatham Islands Standard Time';
-$lang['UP13']	= '(UTC +13:00) Samoa Time Zone, Phoenix Islands Time, Tonga';
-$lang['UP14']	= '(UTC +14:00) Line Islands';
+$lang['UM10']	= '(UTC -10:00) Horário Padrão do Havaí-Aleutas, Ilhas Cook, Taiti';
+$lang['UM95']	= '(UTC -9:30) Ilhas Marquesas';
+$lang['UM9']	= '(UTC -9:00) Horário Padrão do Alasca, Ilhas Gambier';
+$lang['UM8']	= '(UTC -8:00) Horário Padrão do Pacífico, Ilha Clipperton';
+$lang['UM7']	= '(UTC -7:00) Horário Padrão das Montanhas Rochosas';
+$lang['UM6']	= '(UTC -6:00) Horário Padrão Central';
+$lang['UM5']	= '(UTC -5:00) Horário Padrão do Leste, Horário Padrão do Caribe Ocidental';
+$lang['UM45']	= '(UTC -4:30) Horário Padrão da Venezuela';
+$lang['UM4']	= '(UTC -4:00) Horário Padrão do Atlântico, Horário Padrão do Caribe Oriental';
+$lang['UM35']	= '(UTC -3:30) Horário Padrão de Terra Nova';
+$lang['UM3']	= '(UTC -3:00) Argentina, Brasil, Guiana Francesa, Uruguai';
+$lang['UM2']	= '(UTC -2:00) Geórgia do Sul/Ilhas Sandwich do Sul';
+$lang['UM1']	= '(UTC -1:00) Açores, Ilhas de Cabo Verde';
+$lang['UTC']	= '(UTC) Horário Médio de Greenwich, Horário da Europa Ocidental';
+$lang['UP1']	= '(UTC +1:00) Horário da Europa Central, Horário da África Ocidental';
+$lang['UP2']	= '(UTC +2:00) Horário da África Central, Horário da Europa Oriental, Horário de Kaliningrado';
+$lang['UP3']	= '(UTC +3:00) Horário de Moscou, Horário da África Oriental, Horário Padrão da Arábia';
+$lang['UP35']	= '(UTC +3:30) Horário Padrão do Irã';
+$lang['UP4']	= '(UTC +4:00) Horário Padrão do Azerbaijão, Horário de Samara';
+$lang['UP45']	= '(UTC +4:30) Afeganistão';
+$lang['UP5']	= '(UTC +5:00) Horário Padrão do Paquistão, Horário de Ecaterimburgo';
+$lang['UP55']	= '(UTC +5:30) Horário Padrão da Índia, Horário de Sri Lanka';
+$lang['UP575']	= '(UTC +5:45) Horário do Nepal';
+$lang['UP6']	= '(UTC +6:00) Horário Padrão de Bangladesh, Horário do Butão, Horário de Omsk';
+$lang['UP65']	= '(UTC +6:30) Ilhas Cocos, Mianmar';
+$lang['UP7']	= '(UTC +7:00) Horário de Krasnoyarsk, Camboja, Laos, Tailândia, Vietnã';
+$lang['UP8']	= '(UTC +8:00) Horário Padrão Ocidental Australiano, Horário de Pequim, Horário de Irkutsk';
+$lang['UP875']	= '(UTC +8:45) Horário Padrão Centro-Ocidental Australiano';
+$lang['UP9']	= '(UTC +9:00) Horário Padrão do Japão, Horário Padrão da Coreia, Horário de Yakutsk';
+$lang['UP95']	= '(UTC +9:30) Horário Padrão Central Australiano';
+$lang['UP10']	= '(UTC +10:00) Horário Padrão Oriental Australiano, Horário de Vladivostok';
+$lang['UP105']	= '(UTC +10:30) Ilha Lord Howe';
+$lang['UP11']	= '(UTC +11:00) Horário de Srednekolymsk, Ilhas Salomão, Vanuatu';
+$lang['UP115']	= '(UTC +11:30) Ilha Norfolk';
+$lang['UP12']	= '(UTC +12:00) Fiji, Ilhas Gilbert, Horário de Kamchatka, Horário Padrão da Nova Zelândia';
+$lang['UP1275']	= '(UTC +12:45) Horário Padrão das Ilhas Chatham';
+$lang['UP13']	= '(UTC +13:00) Fuso Horário de Samoa, Horário das Ilhas Fênix, Tonga';
+$lang['UP14']	= '(UTC +14:00) Ilhas Line';
